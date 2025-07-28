2f0856b4918790d9553055cbf54dbe2f160c1f0c
jandelrblx <ronzzdragonfly@gmail.com>
2025-07-28 15:12:20 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jandelrblx/growagarden/refs/heads/main/darkscript"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jandelrblx/growagarden/refs/heads/main/darkscript", true))()
